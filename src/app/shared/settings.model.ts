9710bdbacd3a03381f5c5406d746c43025439879
samuele-cozzi <samuele.cozzi@gmail.com>
2020-04-18 23:41:25 +0200

    addi city in type

--- src/app/shared/settings.model.ts
+++ src/app/shared/settings.model.ts
@@ -3,6 +3,7 @@ import { Timestamp } from '@firebase/firestore-types';
 export class Settings {
     id?: string;
     dark: boolean;
+    city: string;
     text: string;
     algoliaApiKey: string;
     algoliaApplicationId: string;
